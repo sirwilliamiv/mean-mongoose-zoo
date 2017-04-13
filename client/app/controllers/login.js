2116588cc4770a8238c93e9490f100dba3d8ff77
Caitlin Stein <caitlin.teter@gmail.com>
2017-04-13 15:24:56 -0500

    log in and register ogic at MVP on client side

--- client/app/controllers/login.js
+++ client/app/controllers/login.js
@@ -2,9 +2,16 @@ app.controller('LoginCtrl', function($scope, userFact){
   console.log('login controller')
   $scope.login = () => {
     console.log("you cliked the login button")
+    userFact.login($scope.loginUser)
+    .then(() => {
+      console.log("logged in")
+    })
   }
 
   $scope.register = () => {
-    console.log("you cliked the register button")
+    userFact.register($scope.loginUser)
+    .then(() => {
+      console.log("registered")
+    })
   }
 })
--- client/app/factories/users.js
+++ client/app/factories/users.js
@@ -1,11 +1,29 @@
 app.factory('userFact', ($http) => {
   return {
-    register: () => {
-
+    register: (loginInfo) => {
+      return new Promise((resolve, reject) => {
+        $http.post('/api/register', loginInfo)
+        .then((data) => {
+          resolve(data)
+        })
+        .catch((err) =>{
+          console.log("error from factory:", err)
+        })
+      })
     },
 
-    login: () => {
-
+    login: (loginInfo) => {
+      console.log("loginInfo from factory", loginInfo)
+      return new Promise((resolve, reject) => {
+        $http.post(`/api/login`, loginInfo)
+        .then((data) => {
+          console.log("logged in from factory")
+          resolve(data)
+        })
+        .catch((err) => {
+          console.log("err from factory:", err)
+        })
+      })
     }
   }
 })
